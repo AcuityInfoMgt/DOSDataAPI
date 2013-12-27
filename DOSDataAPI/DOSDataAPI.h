7ab518c545a561ef60d9d0e9dad71ff0593ec4e6
Kevin Ferrell <kferrell@gmail.com>
2013-12-26 20:29:08 -0500

    Initial Commit

---
+++ DOSDataAPI/DOSDataAPI.h
@@ -0,0 +1,13 @@
+//
+//  DOSDataAPI.h
+//  DOSDataAPI
+//
+//  Created by Kevin Ferrell on 12/26/13.
+//  Copyright (c) 2013 Acuity Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DOSDataAPI : NSObject
+
+@end
---
+++ DOSDataAPI/DOSDataAPI.m
@@ -0,0 +1,13 @@
+//
+//  DOSDataAPI.m
+//  DOSDataAPI
+//
+//  Created by Kevin Ferrell on 12/26/13.
+//  Copyright (c) 2013 Acuity Inc. All rights reserved.
+//
+
+#import "DOSDataAPI.h"
+
+@implementation DOSDataAPI
+
+@end
