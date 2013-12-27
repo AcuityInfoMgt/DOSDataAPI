210c640b6f1996b7607f8c68c2e3239f35205dc5
Kevin Ferrell <kferrell@gmail.com>
2013-12-27 09:21:46 -0500

    Update query param format

--- DOSDataAPI/DOSSecretaryTravelDataManager.m
+++ DOSDataAPI/DOSSecretaryTravelDataManager.m
@@ -29,10 +29,12 @@
 -(void) getSecretaryTravelForPage:(NSInteger)pageNum success:(APISuccessBlock)successBlock failure:(APIFailureBlock)failureBlock
 {
     // Format the query string
-    NSString *query = [NSString stringWithFormat:@"%@?command=%@&per_page=%ld&page=%ld&fields=%@",kAPIBaseURL,kSecretaryTravelCommand,self.itemsPerPage,pageNum,self.resultFields];
+    //NSString *query = [NSString stringWithFormat:@"%@?command=%@&per_page=%ld&page=%ld&fields=%@",kAPIBaseURL,kSecretaryTravelCommand,self.itemsPerPage,pageNum,self.resultFields];
+    
+    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:@"command",kSecretaryTravelCommand,@"per_page",self.itemsPerPage,@"page",pageNum,@"fields",self.resultFields, nil];
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    [manager GET:query parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [manager GET:kAPIBaseURL parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
         successBlock(responseObject);
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
